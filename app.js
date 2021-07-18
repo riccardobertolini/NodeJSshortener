c8a829d262fa9c12a54ac72822164d950834f342
Riccardo Bertolini <info@riccardobertolini.net>
2021-07-18 17:51:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { getCache, updateCache } = require("./util/cacheUtils");
 
 const app = express();
-const port = 5000;
+const port = 5010;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
